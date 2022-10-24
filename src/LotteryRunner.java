9465255a6fed2f0faf26c27f31d40db8ba71b8be
BT_4N2_24 <rkong2588@bths.edu>
2022-10-24 12:28:18 -0400

    added user input

--- src/LotteryRunner.java
+++ src/LotteryRunner.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class LotteryRunner {
     public static void main(String[] args) {
+
+        // Instance Variables
+        Scanner s = new Scanner(System.in);
+        int userNumber;
+
+        // User Input
+        System.out.println("Enter your lottery pick: ");
+        userNumber = s.nextInt();
 
     }
 }
